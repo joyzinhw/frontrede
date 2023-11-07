e31285a020de0939c01e1ffc28ea1c39e180c487
joyce moura <mileniorapper@gmail.com>
2023-11-07 12:38:26 -0300

    navbar

--- rede/client/rede/src/app/(auth)/Navbar.tsx
+++ rede/client/rede/src/app/(auth)/Navbar.tsx
@@ -4,16 +4,22 @@ import { FaHome } from 'react-icons/fa'; // Importe o ícone desejado
 function Navbar() {
   return (
     <nav className="text-white p-4 flex justify-between" style={{ backgroundColor: "#4E77AD" }}>
-       <div>
-       <ul className="mr-4"> 
-       <li className="ml-4"><FaHome size={50}/><a href='http://localhost:3000/login'></a></li>
-       </ul>
-       </div>
+      
+      <div>
+    <ul className="mr-4"> 
+    <li className="ml-4">
+      <a href="/login">
+      <FaHome size={50} />
+      </a>
+    </li>
+    </ul>
+    </div>
+
         
       <ul className="flex justify-end" style={{ fontWeight: "bold",fontSize: "20px", fontStyle: "italic", alignItems: 'center' }}>
         
-        <li className="ml-4"><a href="/faq">SOBRE</a></li>
-        <li className="ml-4"><a href="/fas">AJUDA</a></li>
+        <li className="ml-4"><a href="/navbar/faq">SOBRE</a></li>
+        <li className="ml-4"><a href="/navbar/faqsection">AJUDA</a></li>
       </ul>
     </nav>
   );
--- rede/client/rede/src/app/(auth)/layout.tsx
+++ rede/client/rede/src/app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './Navbar'; // Certifique-se de importar corretamente o componente Navbar
+import Navbar from './Navbar'; 
 
 function AuthPage({ children }: { children: React.ReactNode }) {
   return (
