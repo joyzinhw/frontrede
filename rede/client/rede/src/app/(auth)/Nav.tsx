7b5fa89e8f7b955cb58319bd7be38121db151301
joyce moura <mileniorapper@gmail.com>
2023-11-12 15:49:48 -0300

    att

---
+++ rede/client/rede/src/app/(auth)/Nav.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaHome } from 'react-icons/fa'; // Importe o ícone desejado
+
+function Nav() {
+  return (
+    <nav className="text-white p-4 flex justify-between" style={{ backgroundColor: "#4E77AD" }}>
+      
+      <div>
+      <ul className="mr-4"> 
+      <li className="ml-4">
+      <a href="/login">
+      <FaHome size={50} />
+      </a>
+      </li>
+      </ul>
+      </div>
+
+        
+      <ul className="flex justify-end" style={{ fontWeight: "bold",fontSize: "20px", fontStyle: "italic", alignItems: 'center' }}>
+        
+        <li className="ml-4"><a href="/navbar/faq">SOBRE</a></li>
+        <li className="ml-4"><a href="/navbar/faqsection">AJUDA</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- rede/client/rede/src/app/(auth)/layout.tsx
+++ rede/client/rede/src/app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Navbar from './Navbar'; 
 
-function AuthPage({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
+import Navbar from './Nav'; 
+function AuthPage({children} : {children:React.ReactNode}){
+   
+    return (
+        <div>
       <Navbar />
       <main className="bg-no-repeat bg-cover flex min-h-screen flex-col items-center justify-center" style={{ backgroundColor: "#4E77AD" }}>
         <form className="flex flex-col bg-white px-6 py-14 rounded-2xl gap-11">
